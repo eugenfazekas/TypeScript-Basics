a420b4f1ba6298580be7594c25171d286f18096b
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-06-17 01:31:00 +0300

    Project01: added commandline argument

--- Project 01/src/index.ts
+++ Project 01/src/index.ts
@@ -1,12 +1,19 @@
 import {GitHubApiService} from './GitHubApiService';
 import {User} from "./User";
 import {Repo} from "./Repo";
+import * as _ from 'lodash';
 
 let svc = new GitHubApiService();
-svc.getUserInfo('eugenfazekas', (user: User) => {
-	console.log(user);
-});
-
-svc.getRepos('eugenfazekas', (repos: Repo[]) => {
-	console.log(repos);
-})
+if(process.argv.length < 3) {
+	console.log('Please pass the username as an argument');
+}else {
+	let username = process.argv[2];	
+	svc.getUserInfo(username, (user: User) => {
+		svc.getRepos(username, (repos: Repo[]) => {
+			let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.forCount * -1]);
+			let filterRepos = _.take(sortedRepos,5);
+			user.repos = filterRepos;
+			console.log(user);
+		})
+	});
+}
